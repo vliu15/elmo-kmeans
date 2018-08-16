74fb43e7f5fde571ec24744fdfdc7d3b7ca85bac
VINCENT LIU <vliu@dbslp1204.uhc.com>
2018-08-16 14:08:03 -0500

    analyze mode

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
 import numpy as np
-import json
-from allennlp.commands.elmo import ElmoEmbedder
 
 from util import embed, tokenize
 from sif import sif
@@ -9,6 +7,7 @@ from tsne import tsne
 from cluster import meanshift, dbscan, optics, kmeans
 from meta import write_meta
 from tensorboard import tensorboard
+from analyze import write_groups
 
 import argparse, os
 
@@ -21,38 +20,39 @@ glove_word_file = os.path.join(os.getcwd(), "model", "glove.840B.300d.txt")
 glove_char_file = os.path.join(os.getcwd(), "model", "glove.840B.300d-char.txt")
 
 # output files
-output_dir = os.path.join(os.getcwd(), "output", "samples")
+output_dir = os.path.join(os.getcwd(), "output", "l3-sum")
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 sentence_file = os.path.join(output_dir, "sentences.txt")
-embedding_file = os.path.join(output_dir, "embeddings.npy")
+# embedding_file = os.path.join(output_dir, "embeddings.npy")
+embedding_file = os.path.join(output_dir, "embeddings_sif.npy")
 sif_file = os.path.join(output_dir, "embeddings_sif.npy")
 tsne_file = os.path.join(output_dir, "embeddings_ts.npy")
 ms_labels_file = os.path.join(output_dir, "ms_labels.json")
 db_labels_file = os.path.join(output_dir, "db_labels.json")
 op_labels_file = os.path.join(output_dir, "op_labels.json")
-km_labels_file = os.path.join(output_dir, "km_labels.json")
+km_labels_file = os.path.join(output_dir, "topic_labels.json")
 metadata_file = os.path.join(output_dir, "metadata.tsv")
 
 
 # flags for adjusting parameters at runtime
 flags = tf.flags
 
-flags.DEFINE_string("mode", "embed", "embed, sif, tsne, cluster, metadata, or tensorboard")
+flags.DEFINE_string("mode", "embed", "embed, sif, tsne, cluster, metadata, tensorboard, analyze")
 
 # embedding
-flags.DEFINE_boolean("elmo", True, "use ELMo for embeddings")
+flags.DEFINE_boolean("elmo", False, "use ELMo for embeddings")
 flags.DEFINE_string("elmo_options_file", elmo_options_file, "options file for ELMo embedding")
 flags.DEFINE_string("elmo_weights_file", elmo_weights_file, "weights file for ELMo embedding")
 
-flags.DEFINE_boolean("glove", False, "use GloVe for embeddings")
+flags.DEFINE_boolean("glove", True, "use GloVe for embeddings")
 flags.DEFINE_string("glove_word_file", glove_word_file, "word file for GloVe embedding")
 flags.DEFINE_string("glove_char_file", glove_char_file, "char file for GloVe embedding")
 
 flags.DEFINE_integer("bilm_layer_index", 2, "which bilm layer of ELMo to use, indexed from 0 (-1 for average)")
 flags.DEFINE_boolean("sum_word_vecs", False, "sum word vectors in the same sentence")
 flags.DEFINE_boolean("avg_word_vecs", False, "average word vectors in same sentence")
-flags.DEFINE_boolean("concat_word_vecs", True, "concatenate word vectors in the same sentence")
+flags.DEFINE_boolean("concat_word_vecs", False, "concatenate word vectors in the same sentence")
 flags.DEFINE_integer("max_transcript_len", 30, "if concatenating, length to pad/truncate to") # trans/sent, 75/30
 
 # sif
@@ -82,15 +82,19 @@ flags.DEFINE_boolean("kmeans", True, "use KMeans clustering")
 flags.DEFINE_integer("km_n_clusters", 50, "n_clusters in KMeans function")
 flags.DEFINE_integer("km_n_init", 10, "n_init in KMeans function")
 flags.DEFINE_integer("km_max_iter", 300, "max_iter in KMeans function")
+flags.DEFINE_boolean("km_verbose", True, "verbose in KMeans function")
 flags.DEFINE_integer("km_n_jobs", -1, "n_jobs in KMeans function")
 flags.DEFINE_string("km_algorithm","auto", "algorithm in KMeans function")
 
 # metadata
-flags.DEFINE_boolean("meta_labels", False, "use labels in metadata")
-flags.DEFINE_string("meta_labels_file", db_labels_file, "labels file to be used in metadata")
+flags.DEFINE_boolean("meta_labels", True, "use labels in metadata")
+flags.DEFINE_string("meta_labels_file", km_labels_file, "labels file to be used in metadata")
 
 # tensorboard
 flags.DEFINE_string("log_dir", output_dir, "log directory for TensorBoard")
+
+# analyze
+flags.DEFINE_string("write_dir", os.path.join(output_dir, "groups"), "directory for writing groups")
 
 # files
 flags.DEFINE_string("sentence_file", sentence_file, "file for embedding")
@@ -111,7 +115,10 @@ if __name__ == "__main__":
     if params.mode == "embed":
 
         # tokenize transcriptions
-        tokenized = tokenize(params)
+        # tokenized = tokenize(params)
+
+        with open(params.sentence_file, 'r') as f:
+            tokenized = f.read().splitlines()
 
         # set up TF with GPU usage
         config = tf.ConfigProto(allow_soft_placement=True)
@@ -150,3 +157,8 @@ if __name__ == "__main__":
 
         # set up tensorboard projector
         tensorboard(params)
+
+    if params.mode == "analyze":
+
+        # write groups of sentences from clusters
+        write_groups(params)
