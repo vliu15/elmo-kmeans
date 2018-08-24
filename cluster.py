f47225cf20c1cf9d203875a327e0673341d07df2
VINCENT LIU <vliu@dbslp1204.uhc.com>
2018-08-24 14:53:17 -0500

    remove DBSCAN

--- cluster.py
+++ cluster.py
@@ -2,32 +2,9 @@ import numpy as np
 import json
 import math
 
-from sklearn.cluster import DBSCAN, KMeans
+from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from tqdm import *
-
-
-def dbscan(params):
-    '''
-    Cluster arrays using DBSCAN.
-    :params.embedding_file: 2D NumPy arrays
-    '''
-
-    vectors = np.load(params.embedding_file)
-
-    db = DBSCAN(eps=params.db_eps,
-                min_samples=params.db_min_samples,
-                metric=params.db_metric,
-                algorithm=params.db_algorithm,
-                n_jobs=params.db_n_jobs)
-
-    labels = db.fit_predict(vectors)
-    print("Number of estimated clusters using DBSCAN: %d\n" % (len(set(labels)) - (1 if -1 in labels else 0)))
-    print("Noise: %d\n" %  len([i for i in labels if i == -1]))
-
-    # write list of labels to output
-    with open(params.db_labels_file, 'w') as f:
-        json.dump(labels.tolist(), f)
 
 
 def kmeans(params):
@@ -38,40 +15,54 @@ def kmeans(params):
 
     vectors = np.load(params.embedding_file)
 
-    if params.km_opt:
-        opt = []
-        incr = int((params.max_k - params.min_k) / params.n_k)
+    km = KMeans(n_clusters=params.n_clusters,
+                n_init=params.n_init,
+                max_iter=params.max_iter,
+                verbose=params.verbose,
+                n_jobs=params.n_jobs,
+                algorithm=params.algorithm).fit(vectors)
+    labels = km.labels_
+    inertia = km.inertia_
+    silhouette = silhouette_score(vectors, labels)
+    print("Clusters: %d \t Inertia: %.5f \t 'silhouette: %.5f\n" % (params.n_clusters, inertia, silhouette))
 
-        # loop through different k values
-        for i in tqdm(range(params.n_k + 1)):
-            cur_n = params.max_k - i * incr
-            km = KMeans(n_clusters=cur_n,
-                        n_init=params.km_n_init,
-                        max_iter=params.km_max_iter,
-                        verbose=params.km_verbose,
-                        n_jobs=params.km_n_jobs,
-                        algorithm=params.km_algorithm).fit(vectors)
-            labels = km.labels_
-            inertia = km.inertia_
-            silhouette = silhouette_score(vectors, labels)
-            opt.append(str(cur_n) + '\t' + str(inertia) + '\t' + str(silhouette) + '\n')
-            print("Clusters: %d \t Inertia: %.5f \t Silhouette: %.5f \n" % (cur_n, inertia, silhouette))
+    # write list of labels to output
+    with open(params.km_labels_file, 'w') as f:
+        json.dump(labels.tolist(), f)
 
-        with open(params.km_opt_file, 'w') as f:
-            f.writelines(opt)
+def opt_k(params):
+    '''
+    Returns inertia and silhouette scores of k clusters in specified range.
+    :params.embedding_file: 2D NumPy arrays
+    :params.min_k: minimum k value to start
+    :params.max_k: maximum k value to start
+    :params.n_k: number of k values to try within range
+    '''
 
-    else:
-        km = KMeans(n_clusters=params.km_n_clusters,
-                    n_init=params.km_n_init,
-                    max_iter=params.km_max_iter,
-                    verbose=params.km_verbose,
-                    n_jobs=params.km_n_jobs,
-                    algorithm=params.km_algorithm).fit(vectors)
+    vectors = np.load(params.embedding_file)
+
+    opt = []
+    incr = int((params.max_k - params.min_k) / params.n_k)
+
+    # loop through different k values
+    for i in tqdm(range(params.n_k + 1)):
+        cur_n = params.max_k - i * incr
+        km = KMeans(n_clusters=cur_n,
+                    n_init=params.n_init,
+                    max_iter=params.max_iter,
+                    verbose=params.verbose,
+                    n_jobs=params.n_jobs,
+                    algorithm=params.algorithm).fit(vectors)
         labels = km.labels_
         inertia = km.inertia_
         silhouette = silhouette_score(vectors, labels)
-        print("Clusters: %d \t Inertia: %.5f \t 'silhouette: %.5f\n" % (params.km_n_clusters, inertia, silhouette))
+        opt.append(str(cur_n) + '\t' + str(inertia) + '\t' + str(silhouette) + '\n')
+        print("Clusters: %d \t Inertia: %.5f \t Silhouette: %.5f \n" % (cur_n, inertia, silhouette))
 
-        # write list of labels to output
-        with open(params.km_labels_file, 'w') as f:
-            json.dump(labels.tolist(), f)
+    with open(params.km_opt_file, 'w') as f:
+        f.writelines(opt)
+
+
+def hierarch_k(params):
+
+    vectors = np.load(params.embedding_file)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from util import embed, tokenize
 from sif import sif
-from cluster import dbscan, kmeans
+from cluster import kmeans, opt_k, hierarch_k
 from analyze import write_groups, remove_groups
 from project import pca, tsne
 from meta import write_meta
@@ -20,7 +20,7 @@ glove_word_file = os.path.join(os.getcwd(), "model", "glove.840B.300d.txt")
 glove_char_file = os.path.join(os.getcwd(), "model", "glove.840B.300d-char.txt")
 
 # output files
-output_dir = os.path.join(os.getcwd(), "output", "l3-avg", "trimmed")
+output_dir = os.path.join(os.getcwd(), "output", "l3-avg", "trimmed" )
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 sentence_file = os.path.join(output_dir, "sentences.txt")
@@ -29,7 +29,6 @@ embedding_file = os.path.join(output_dir, "embeddings_sif.npy")
 sif_file = os.path.join(output_dir, "embeddings_sif.npy")
 pca_file = os.path.join(output_dir, "embeddings_pc.npy")
 tsne_file = os.path.join(output_dir, "embeddings_ts.npy")
-db_labels_file = os.path.join(output_dir, "db_labels.json")
 km_labels_file = os.path.join(output_dir, "km_labels-25.json")
 km_opt_file = os.path.join(output_dir, "km_opt.csv")
 metadata_file = os.path.join(output_dir, "metadata.tsv")
@@ -42,7 +41,6 @@ flags.DEFINE_string("sentence_file", sentence_file, "file for embedding")
 flags.DEFINE_string("embedding_file", embedding_file, "file for sentence embeddings")
 flags.DEFINE_string("sif_file", sif_file, "file for SIF embeddings")
 flags.DEFINE_string("tsne_file", tsne_file, "file for t-SNE embeddings")
-flags.DEFINE_string("db_labels_file", db_labels_file, "file for DBSCAN cluster labels")
 flags.DEFINE_string("km_labels_file", km_labels_file, "file for KMeans cluster labels")
 flags.DEFINE_string("km_opt_file", km_opt_file, "file for KMeans cluster-inertia-silhouette output")
 flags.DEFINE_string("metadata_file", metadata_file, "file for TensorBoard metadata")
@@ -54,6 +52,7 @@ flags.DEFINE_string("mode", "embed", "embed, sif, cluster, analyze, project, met
 flags.DEFINE_boolean("elmo", True, "use ELMo for embeddings")
 flags.DEFINE_string("elmo_options_file", elmo_options_file, "options file for ELMo embedding")
 flags.DEFINE_string("elmo_weights_file", elmo_weights_file, "weights file for ELMo embedding")
+flags.DEFINE_integer("elmo_cuda_device", -1, "GPU device to run on")
 
 flags.DEFINE_boolean("glove", False, "use GloVe for embeddings")
 flags.DEFINE_string("glove_word_file", glove_word_file, "word file for GloVe embedding")
@@ -61,33 +60,31 @@ flags.DEFINE_string("glove_char_file", glove_char_file, "char file for GloVe emb
 
 flags.DEFINE_integer("bilm_layer_index", 2, "which bilm layer of ELMo to use, indexed from 0 (-1 for average)")
 flags.DEFINE_boolean("sum_word_vecs", False, "sum word vectors in the same sentence")
-flags.DEFINE_boolean("avg_word_vecs", True, "average word vectors in same sentence")
+flags.DEFINE_boolean("avg_word_vecs", False, "average word vectors in same sentence")
 flags.DEFINE_boolean("concat_word_vecs", False, "concatenate word vectors in the same sentence")
+flags.DEFINE_boolean("max_pool_word_vecs", True, "max pooling across word vectors in the same sentence")
 flags.DEFINE_integer("max_transcript_len", 30, "if concatenating, length to pad/truncate to") # trans/sent, 75/30
 
 # sif
 flags.DEFINE_integer("sif_rmpc", 1, "number of principal components to remove")
 
 # cluster
-flags.DEFINE_boolean("dbscan", False, "use DBSCAN clustering")
-flags.DEFINE_float("db_eps", 1, "eps in DBSCAN function")
-flags.DEFINE_integer("db_min_samples", 10, "min_samples in DBSCAN function")
-flags.DEFINE_string("db_metric", "euclidean", "metric in DBSCAN function")
-flags.DEFINE_string("db_algorithm", "brute", "algorithm in DBSCAN function")
-flags.DEFINE_integer("db_n_jobs", -1, "n_jobs in DBSCAN function")
-
 flags.DEFINE_boolean("kmeans", True, "use KMeans clustering")
-flags.DEFINE_integer("km_n_clusters", 25, "n_clusters in KMeans function")
-flags.DEFINE_integer("km_n_init", 10, "n_init in KMeans function")
-flags.DEFINE_integer("km_max_iter", 300, "max_iter in KMeans function")
-flags.DEFINE_boolean("km_verbose", False, "verbose in KMeans function")
-flags.DEFINE_integer("km_n_jobs", -1, "n_jobs in KMeans function")
-flags.DEFINE_string("km_algorithm","auto", "algorithm in KMeans function")
+flags.DEFINE_integer("n_clusters", 25, "n_clusters in KMeans function")
+flags.DEFINE_integer("n_init", 10, "n_init in KMeans function")
+flags.DEFINE_integer("max_iter", 300, "max_iter in KMeans function")
+flags.DEFINE_boolean("verbose", False, "verbose in KMeans function")
+flags.DEFINE_integer("n_jobs", -1, "n_jobs in KMeans function")
+flags.DEFINE_string("algorithm","auto", "algorithm in KMeans function")
 
-flags.DEFINE_boolean("km_opt", False, "find optimal k")
-flags.DEFINE_integer("min_k", 5, "minimum k to try")
-flags.DEFINE_integer("max_k", 155, "maximum k to try")
+flags.DEFINE_boolean("opt_k", False, "find optimal k")
+flags.DEFINE_integer("min_k", 10, "minimum k to try")
+flags.DEFINE_integer("max_k", 160, "maximum k to try")
 flags.DEFINE_integer("n_k", 15, "number of k's to try")
+
+flags.DEFINE_boolean("hierarch_k", False, "compute kmeans hierarchically")
+flags.DEFINE_integer("split_size", 2, "number of clusters at each level")
+flags.DEFINE_integer("n_iter", 7, "number of levels of hierarchy")
 
 # analyze
 flags.DEFINE_string("group_labels_file", km_labels_file, "labels file to write sentence groups")
@@ -100,7 +97,7 @@ flags.DEFINE_string("trim_dir", os.path.join(output_dir, "trimmed"), "directory 
 flags.DEFINE_boolean("pca", False, "use pca for visualization")
 flags.DEFINE_integer("pc_n_components", 3, "n_components in PCA function")
 
-flags.DEFINE_boolean("tsne", True, "us tsne for visualization")
+flags.DEFINE_boolean("tsne", True, "use tsne for visualization")
 flags.DEFINE_integer("ts_n_components", 3, "n_components in TSNE function")
 flags.DEFINE_integer("ts_perplexity", 50, "perplexity n TSNE function")
 flags.DEFINE_integer("ts_learning_rate", 10, "learning_rate in TSNE function")
@@ -125,11 +122,13 @@ if __name__ == "__main__":
             tokenized = f.read().splitlines()
 
         # set up TF with GPU usage
+        with tf.device('/device:GPU:1'):
+            emb = embed(params, tokenized)
         config = tf.ConfigProto(allow_soft_placement=True)
         config.gpu_options.allow_growth = True
 
         with tf.Session(config=config) as sess:
-            emb = sess.run(embed(params, tokenized))
+            emb = sess.run(emb)
             print("Saving embeddings...\n")
             np.save(params.embedding_file, emb)
 
@@ -137,10 +136,12 @@ if __name__ == "__main__":
         sif(params)
 
     if params.mode == "cluster":
-        if params.dbscan:
-            dbscan(params)
         if params.kmeans:
             kmeans(params)
+        if params.opt_k:
+            opt_k(params)
+        if params.hierarch_k:
+            hierarch_k(params)
 
     if params.mode == "analyze":
         if params.write_groups:
